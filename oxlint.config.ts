2e00ee553f20a96e34bf6da37b4c7db8e577b884
My Name is Tito <github@mynameistito.com>
2026-08-24 23:37:33 +1200

    ultracite: add anti-slop, eslint-plugin-github, eslint-plugin-sonarjs plugins

--- oxlint.config.ts
+++ oxlint.config.ts
@@ -1,8 +1,8 @@
 import { defineConfig } from "oxlint";
-import core from "ultracite/oxlint/core";
-import solid from "ultracite/oxlint/solid";
 import antiSlop from "ultracite/oxlint/anti-slop";
+import core from "ultracite/oxlint/core";
 import { selectJsPlugins } from "ultracite/oxlint/js-plugins";
+import solid from "ultracite/oxlint/solid";
 
 export default defineConfig({
   extends: [core, solid, antiSlop, selectJsPlugins(["github", "sonarjs"])],
